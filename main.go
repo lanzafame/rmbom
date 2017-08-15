4781c16045993a3687645ec10ad4853353792fe9
Adrian Lanzafame <adrianlanzafame92@gmail.com>
2017-08-16 09:23:42 +1000

    clean up the code

--- main.go
+++ main.go
@@ -2,27 +2,42 @@ package main
 
 import (
 	"bytes"
+	"flag"
 	"io/ioutil"
 	"log"
 	"os"
 )
 
 func main() {
-	if os.Args[1] == "" {
-		log.Fatalln("please provide file path")
+	var (
+		input  = flag.String("i", "", "the `input file` to remove the BOM from")
+		output = flag.String("o", "", "the name of the `output file`")
+		bom    = []byte("\xef\xbb\xbf")
+	)
+
+	flag.Parse()
+	if flag.NFlag() < 1 || *input == "" {
+		flag.Usage()
+		os.Exit(-1)
 	}
-	fb, err := ioutil.ReadFile()
+
+	fb, err := ioutil.ReadFile(*input)
 	if err != nil {
 		log.Fatalf("failed to read file: %v", err)
 	}
 
-	bom := []byte("\xef\xbb\xbf")
-
 	if !bytes.Equal(bom, fb[0:3]) {
-		// short circuit
+		// short circuit if file doesn't have bom
 		return
 	}
-	err = ioutil.WriteFile(*i, fb[3:], 0666)
+
+	// default to overwriting input file, unless output flag was set
+	filename := *input
+	if *output != "" {
+		filename = *output
+	}
+
+	err = ioutil.WriteFile(filename, fb[3:], 0666)
 	if err != nil {
 		log.Fatalf("failed to write file: %v", err)
 	}
